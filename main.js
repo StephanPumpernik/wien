94af1c7e57d9425f860c613d588c2a7b8687409d
StephanPumpernik <stephan.pumpernik@gmx.net>
2025-04-03 18:02:58 +0200

    GeoJSON daten in die overlays schreiben

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ async function loadSights(url) {
     //console.log(jsondata);
     L.geoJSON(jsondata, {
         attribution: "Datenquelle: <a href= 'Stadt Wien' https://data.wien.gv.at> Stadt Wien <a/>"
-    }).addTo(map);
+    }).addTo(overlays.sights);
 }
 
 // Sehenswürdigkeiten Linien
@@ -56,7 +56,7 @@ async function loadLines(url) {
     //console.log(jsondata);
     L.geoJSON(jsondata, {
         attribution: "Datenquelle: <a href= 'Stadt Wien' https://data.wien.gv.at> Stadt Wien <a/>"
-    }).addTo(map);
+    }).addTo(overlays.lines);
 }
 
 
@@ -68,7 +68,7 @@ async function loadStops(url) {
     //console.log(jsondata);
     L.geoJSON(jsondata, {
         attribution: "Datenquelle: <a href= 'Stadt Wien' https://data.wien.gv.at> Stadt Wien <a/>"
-    }).addTo(map);
+    }).addTo(overlays.stops);
 }
 
 
@@ -80,7 +80,7 @@ async function loadZones(url) {
     //console.log(jsondata);
     L.geoJSON(jsondata, {
         attribution: "Datenquelle: <a href= 'Stadt Wien' https://data.wien.gv.at> Stadt Wien <a/>"
-    }).addTo(map);
+    }).addTo(overlays.zones);
 }
 loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
 loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
