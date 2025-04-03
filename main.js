766345397a5fb31886ca59ca62d57ecebc3e0e52
StephanPumpernik <stephan.pumpernik@gmx.net>
2025-04-03 17:39:06 +0200

    leere layercontrol erzeugt

--- main.js
+++ main.js
@@ -17,6 +17,13 @@ L.tileLayer('https://mapsneu.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{
     attribution: 'Hintergrundkarte: <a href="https://www.basemap.at">basemap.at</a>'
 }).addTo(map);
 
+//Layercontrol
+L.control.layers({
+
+}, {
+
+}).addTo(map);
+
 //maßstab machen
 L.control.scale({
     imperial: false
